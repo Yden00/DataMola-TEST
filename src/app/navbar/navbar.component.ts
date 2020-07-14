ebad35cdefe1f934503228597e619c756286c8d4
ue4prog <vladtet2000@mail.ru>
2020-07-14 18:42:32 +0300

    feat: add table soring and filtering

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -8,10 +8,25 @@ import { ELEMENT_DATA } from '../../assets/dataSource';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  public name = new FormControl('');
+  public genre = new FormControl('');
+  public year = new FormControl('');
+
+  public genres = [
+    "drama",
+    "horror",
+    "crime",
+    "dark comedy",
+    "tragedy",
+  ]
+  public years = [
+    "05.11.2019",
+    "01.01.2019",
+    "20.10.2019"
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
--- src/app/table-component/table-component.component.ts
+++ src/app/table-component/table-component.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, ViewChild} from '@angular/core';
+import { Component, OnInit, ViewChild, Input} from '@angular/core';
 import { ELEMENT_DATA } from '../../assets/dataSource';
 import { MatPaginator } from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';
 import {ThemePalette} from '@angular/material/core';
 import {MatSort} from '@angular/material/sort';
-import { DataSource } from '@angular/cdk/table';
+import { Observable, combineLatest } from 'rxjs';
+import { startWith } from 'rxjs/operators'
 
 @Component({
   selector: 'app-table-component',
@@ -13,17 +14,30 @@ import { DataSource } from '@angular/cdk/table';
 })
 
 export class TableComponentComponent implements OnInit {
+  @Input() yearFilter: Observable<string>;
+  @Input() nameFilter: Observable<string>;
+  @Input() genreFilter: Observable<string>;
+
   public displayedColumns: string[] = ["name", "season", "network", "premiere"]
   dataSource = new MatTableDataSource(ELEMENT_DATA);
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: true}) sort: MatSort;
   constructor() {}
   ngOnInit(){
-    this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
     this.dataSource = this.dataSource;
+    combineLatest([
+      this.yearFilter.pipe(startWith('')),
+      this.nameFilter.pipe(startWith('')),
+      this.genreFilter.pipe(startWith(''))
+    ])
+      .subscribe(([year, name, genre]) => {
+        this.dataSource = new MatTableDataSource(ELEMENT_DATA.filter(element => {
+          return element.name.includes(name) && (element.chips.includes(genre) || !genre) && element.premiere.includes(year);
+        }))
+      this.dataSource.paginator = this.paginator;
+      })
   }
-
 }
 export interface ChipColor {
   name: string;
